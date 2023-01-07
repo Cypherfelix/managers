f8a61f2ece2c270ced38bc59ac4c9d3ead4e24ca
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-07 21:47:42 +0300

    Sidebar creation plus css

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
 
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">Container</div>
+    </div>
   );
 };
 
