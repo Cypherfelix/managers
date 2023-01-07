f9e6c59fc72bcd768ef82b76d9f925034ae5e984
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-07 22:12:50 +0300

    Navbar creation together with styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
+import "./home.scss"
 import Sidebar from "../../components/sidebar/Sidebar";
-import "./home.scss";
+import Navbar from "../../components/navbar/Navbar";
 
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">Container</div>
+      <div className="homeContainer">
+        <Navbar />
+        homeContainer
+      </div>
     </div>
   );
 };
