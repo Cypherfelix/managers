54bbd7e05d1ef3761edc71b38d4eff072193dcf6
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-08 18:21:52 +0300

    Adding react dom links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,12 +12,14 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">Admin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Admin</span></Link>
       </div>
       <hr />
 
@@ -30,15 +32,23 @@ const Sidebar = () => {
           </li>
           <p className="title">LISTS</p>
 
-          <li>
-            <PersonOutlineIcon className="icon" />
-            <span>Users</span>
-          </li>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
 
-          <li>
-            <StoreIcon className="icon" />
-            <span>Products</span>
-          </li>
+          </Link>
+
+          <Link to="/orders" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Products</span>
+            </li>
+
+          </Link>
+
+
           <li>
             <CreditCardIcon className="icon" />
             <span>Orders</span>
