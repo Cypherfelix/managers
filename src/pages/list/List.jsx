d30301b058c3e560666387505b773025f63b7d27
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-08 17:47:33 +0300

    React MUI data table

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,16 @@
 import "./list.scss"
+import Datatable from "../../components/datatable/Datatable"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
 
 const List = () => {
   return (
     <div className="list">
-      list
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
     </div>
   )
 }
