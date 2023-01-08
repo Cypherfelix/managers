cd0207b0ba6d239d4160dbd957c9f2c586fe9c42
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-08 11:19:01 +0300

    React MUI table creation on the dashboard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
+
 
 
 const Home = () => {
@@ -22,6 +24,12 @@ const Home = () => {
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
+
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+
       </div>
     </div>
   );
